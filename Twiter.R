8f678b7429dea55d630d3c95fa4a6ccd5d6d0ee7
Pablo E. Gutierrez Fonseca <pabloe.gutierrezfonseca@gmail.com>
2020-05-01 22:38:43 -0600

    Research

---
+++ Twiter.R
@@ -0,0 +1,18 @@
+
+
+<section id="main-description">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8">
+        ...
+      </div>   
+        <div class="col-md-4">
+          <div>
+          <a class="twitter-timeline" href="https://twitter.com/pabloguticr"  data-width="300"
+          data-height="300" data-tweet-limit="4">Tweets by Pablo</a>
+            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            </div>
+            </div>
+            </div>
+            </div>
+            </section
